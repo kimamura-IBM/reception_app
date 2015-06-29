a249aa935536d0b625927bbf3a9f8ed3c3badc26
Herrokkin <k1@herrokkin.jp>
2015-06-29 20:25:38 +0900

    Fixed form_tag

--- config/initializers/slack_bot.rb
+++ config/initializers/slack_bot.rb
@@ -1,6 +1,6 @@
 SlackBot.setup do |config|
       config.token = 'xoxp-6978191347-6977886208-6978512406-850017'
-      config.channel = 'Herrokkin'
+      config.channel = '#general'
       config.bot_name = 'SlackBot'
-      config.body = 'MESSAGE BY SlackBot'
+      config.body = 'MESSAGE BY SlackBot via Rails'
 end
