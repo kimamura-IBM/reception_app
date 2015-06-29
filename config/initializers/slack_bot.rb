6a1bc6b44650fff9425d00ca6f9c40c4daf678bd
Herrokkin <k1@herrokkin.jp>
2015-06-29 19:30:19 +0900

    Added SlackBot

---
+++ config/initializers/slack_bot.rb
@@ -0,0 +1,6 @@
+SlackBot.setup do |config|
+      config.token = 'xoxp-6978191347-6977886208-6978512406-850017'
+      config.channel = 'Herrokkin'
+      config.bot_name = 'SlackBot'
+      config.body = 'MESSAGE BY SlackBot'
+end
