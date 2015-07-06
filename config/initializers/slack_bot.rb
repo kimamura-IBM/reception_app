2d187c1f2873224c4cecf61e8dee25bf4bf2588f
Herrokkin <k1@herrokkin.jp>
2015-07-06 14:55:02 +0900

    Minor fix

--- config/initializers/slack_bot.rb
+++ config/initializers/slack_bot.rb
@@ -3,6 +3,6 @@ SlackBot.setup do |config|
       config.token = 'xoxp-6978191347-6977886208-6978512406-850017' #Herrokkin
       #config.channel = '#read_only' #n2p
       config.channel = '#general' #Herrokkin
-      config.bot_name = 'SlackBot'
-      config.body = '受付Webアプリのテスト送信です。 https://damp-reaches-2263.herokuapp.com/ https://github.com/Herrokkin/twilio-tutorial-clicktocall-rails/'
+      config.bot_name = 'Uketsuke-App'
+      config.body = '受付Webアプリのテスト送信です。 https://github.com/Herrokkin/twilio-tutorial-clicktocall-rails/'
 end
