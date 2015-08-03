75e04be7ec2001e2cc93ecfa91a9073ede403092
Herrokkin <k1@herrokkin.jp>
2015-08-03 20:51:59 +0900

    Fixed business time

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -5,3 +5,5 @@ BusinessTime::Config.load("#{Rails.root}/config/business_time.yml")
 #  BusinessTime::Config.end_of_workday = "11:30 am"
 #  BusinessTime::Config.holidays << Date.parse("August 4th, 2010")
 
+ BusinessTime::Config.beginning_of_workday = "10:30 am"
+ BusinessTime::Config.end_of_workday = "8:30 pm"
