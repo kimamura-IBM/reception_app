6f35fcd7b1c57ec50aa8ea7cc9a41929570bf1ad
Herrokkin <k1@herrokkin.jp>
2015-08-08 15:13:54 +0900

    Revert b5723d5..6cf64e5
    This rolls back to commit b5723d5c2a81efb2f910d72de48885355381751a.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,7 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-AppCondition.create(:status => 2, :user => 'test', :reason => '休日出勤')
-AppCondition.create(:status => 3, :user => 'test', :reason => '臨時休業')
-AppCondition.create(:status => 1, :user => 'test', :reason => '通常に戻す')
